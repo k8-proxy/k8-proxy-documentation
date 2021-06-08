f73526cc6f8e214ca587a78c1172cf414c406606
Shereen <sh.alaa@nu.edu.eg>
2021-06-08 13:25:25 +0200

    added upgrade

--- sidebars.auto.js
+++ sidebars.auto.js
@@ -34,6 +34,7 @@ module.exports = {
                     "products/gw cloud sdk/d-desktop-integration",
                     "products/gw cloud sdk/deployment",
                     "products/gw cloud sdk/python-sample",
+                    "products/gw cloud sdk/upgrade",
                     "products/gw cloud sdk/zip_scenarios"
                 ]
             },
